87988ff976b7880c5a8d277a1ba9af8cd21a609b
Mr Mystery <105168056+Mr-Mystery-13@users.noreply.github.com>
2023-06-03 20:44:38 -0700

    animate

--- main.js
+++ main.js
@@ -52,18 +52,18 @@ document.querySelector("#hacked").onmouseover = (event) => {
 // Text Morph
 
 const elts = {
-	text1: document.getElementById("text-morph1"),
-	text2: document.getElementById("text-morph2")
+    text1: document.getElementById("text-morph1"),
+    text2: document.getElementById("text-morph2")
 };
 
 const texts = [
-	"Why",
-	"is",
-	"this",
-	"so",
-	"satisfying",
-	"to",
-	"watch?"
+    "Why",
+    "is",
+    "this",
+    "so",
+    "satisfying",
+    "to",
+    "watch?"
 ];
 
 const morphTime = 1;
@@ -77,60 +77,59 @@ let cooldown = cooldownTime;
 elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
-function doMorph() {
-	morph -= cooldown;
-	cooldown = 0;
-	
-	let fraction = morph / morphTime;
-	
-	if (fraction > 1) {
-		cooldown = cooldownTime;
-		fraction = 1;
-	};
-	
-	setMorph(fraction);
-};
-
-function setMorph(fraction) {
-	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-	
-	fraction = 1 - fraction;
-	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-	
-	elts.text1.textContent = texts[textIndex % texts.length];
-	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-};
-
-function doCooldown() {
-	morph = 0;
-	
-	elts.text2.style.filter = "";
-	elts.text2.style.opacity = "100%";
-	
-	elts.text1.style.filter = "";
-	elts.text1.style.opacity = "0%";
-};
-
 function animate() {
-	requestAnimationFrame(animate);
-	
-	let newTime = new Date();
-	let shouldIncrementIndex = cooldown > 0;
-	let dt = (newTime - time) / 1000;
-	time = newTime;
-	
-	cooldown -= dt;
-	
-	if (cooldown <= 0) {
-		if (shouldIncrementIndex) {
-			textIndex++;
-		};
-		doMorph();
-	} else {
-		doCooldown();
-	};
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+
+        if (shouldIncrementIndex) {
+            textIndex++;
+        };
+
+        // doMorph
+
+        morph -= cooldown;
+        cooldown = 0;
+
+        let fraction = morph / morphTime;
+
+        if (fraction > 1) {
+            cooldown = cooldownTime;
+            fraction = 1;
+        };
+
+        // setMorph
+
+        elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+        elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+        fraction = 1 - fraction;
+        elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+        elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+        elts.text1.textContent = texts[textIndex % texts.length];
+        elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+    } else {
+
+        // doCooldown
+
+        morph = 0;
+
+        elts.text2.style.filter = "";
+        elts.text2.style.opacity = "100%";
+
+        elts.text1.style.filter = "";
+        elts.text1.style.opacity = "0%";
+
+    };
 };
 
 animate();
